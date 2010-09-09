8adcde1b29c9f28d95a91bc4ae1acbd558b9eafb
Oliver Lucido <dev@redboxresearchdata.com.au>
2010-09-09 02:19:57 +0000

    refs #1121 #1122 added dynamic opensearch descriptor for each view with urls linking to the view search page and also supporting the suggestions extension

---
+++ src/main/config/portal/default/newcastle/scripts/opensearch/description.py
@@ -0,0 +1,21 @@
+from au.edu.usq.fascinator.common import JsonConfig
+
+class DescriptionData:
+    def __init__(self):
+        pass
+    
+    def __activate__(self, context):
+        self.request = context["request"]
+        self.portalId = context["portalId"]
+        self.request.setAttribute("Content-Type", "application/opensearchdescription+xml")
+        
+        jc = JsonConfig()
+        baseUrl = jc.get("urlBase")
+        if baseUrl.endswith("/"):
+            baseUrl = baseUrl[:-1]
+        self.__baseUrl = baseUrl
+    
+    def getBaseUrl(self):
+        return self.__baseUrl + "/" + self.portalId
+    
+
