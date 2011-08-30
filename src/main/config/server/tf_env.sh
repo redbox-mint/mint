e3885bbfd6e7fe0bafbb57f930027e185e4849ac
Greg Pendlebury <greg.pendlebury@gmail.com>
2011-08-30 04:52:44 +0000

    Fixing typo in shell script

--- src/main/config/server/tf_env.sh
+++ src/main/config/server/tf_env.sh
@@ -49,7 +49,7 @@ fi
 JETTY_OPTS="-Djetty.port=$LOCAL_PORT -Djetty.logs=$JETTY_LOGS -Djetty.home=$PROJECT_HOME/server/jetty"
 
 # solr settings
-SOLR_OPTS="-Dsolr.solr.home=$PROJECT_HOME/solr
+SOLR_OPTS="-Dsolr.solr.home=$PROJECT_HOME/solr"
 
 # Geonames
 GEONAMES="-Dgeonames.solr.home=$PROJECT_HOME/home/geonames/solr"
