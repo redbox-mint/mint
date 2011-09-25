f50b7106a2683a566d56ccbeba42218627374568
Greg Pendlebury <greg.pendlebury@gmail.com>
2011-09-25 23:28:05 +0000

    Adjustments to logging rotation to avoid OS file creation issues.

--- src/main/config/server/jetty/resources/logback.groovy
+++ src/main/config/server/jetty/resources/logback.groovy
@@ -43,7 +43,7 @@ appender("SIFT", GSiftingAppender) {
       append = true
       file = "${logHome}/logs/${name}.log"
       rollingPolicy(TimeBasedRollingPolicy) {
-        fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}/${name}.zip"
+        fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}_${name}.zip"
         maxHistory = 30
       }
       encoder(PatternLayoutEncoder) {
@@ -55,7 +55,7 @@ appender("SIFT", GSiftingAppender) {
 appender("AMQ", RollingFileAppender) {
   file = "${logHome}/logs/amq/amq.log"
   rollingPolicy(TimeBasedRollingPolicy) {
-    fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}/amq/amq.zip"
+    fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}_amq.zip"
     maxHistory = 30
   }
   encoder(PatternLayoutEncoder) {
@@ -65,7 +65,7 @@ appender("AMQ", RollingFileAppender) {
 appender("GEO", RollingFileAppender) {
   file = "${logHome}/logs/geonames/geonames.log"
   rollingPolicy(TimeBasedRollingPolicy) {
-    fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}/geonames/geonames.zip"
+    fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}_geonames.zip"
     maxHistory = 30
   }
   encoder(PatternLayoutEncoder) {
@@ -75,7 +75,7 @@ appender("GEO", RollingFileAppender) {
 appender("SOLR", RollingFileAppender) {
   file = "${logHome}/logs/solr/solr.log"
   rollingPolicy(TimeBasedRollingPolicy) {
-    fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}/solr/solr.zip"
+    fileNamePattern = "${logHome}/logs/archives/%d{yyyy-MM}_solr.zip"
     maxHistory = 30
   }
   encoder(PatternLayoutEncoder) {
